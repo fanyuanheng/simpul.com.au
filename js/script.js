c5637d97db3a695c05749efb122fe456b0ca9c99
Yuan Heng Fan <fanyuanheng@gmail.com>
2013-09-24 10:44:58 +1000

    landing page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+
+	$('.site').videoBG({
+		position:"fixed",
+		zIndex:-1,
+		mp4:'assets/christmas_snow.mp4',
+		ogv:'assets/christmas_snow.ogv',
+		webm:'assets/christmas_snow.webm',
+		poster:'assets/christmas_snow.jpg',
+		opacity:0.5,
+		fullscreen:true,
+		loop:true
+	});
+
+})
